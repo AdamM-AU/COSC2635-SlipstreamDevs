2984121d954e0619588774974a665e6be74a8699
AdamM-AU <70795583+AdamM-AU@users.noreply.github.com>
2022-04-15 21:05:40 +1000

    Update dashboard-home.php

--- Project Source Code/modules/dashboard-home.php
+++ Project Source Code/modules/dashboard-home.php
@@ -13,7 +13,7 @@
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks
+                                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tickets in Progress
                                             </div>
                                             <div class="row no-gutters align-items-center">
                                                 <div class="col-auto">
@@ -36,14 +36,14 @@
                             </div>
                         </div>
 
-                        <!-- Pending Requests Card Example -->
+                        <!-- Pending Tickets -->
                         <div class="col-xl-3 col-md-6 mb-4">
                             <div class="card border-left-warning shadow h-100 py-2">
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                                Pending Requests</div>
+                                                Pending Tickets</div>
                                             <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
                                         </div>
                                         <div class="col-auto">
@@ -53,7 +53,27 @@
                                 </div>
                             </div>
                         </div>
+						
+                        <!-- Unassigned Tickets -->
+                        <div class="col-xl-3 col-md-6 mb-4">
+                            <div class="card border-left-danger shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                                Unassigned Tickets</div>
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">42,100</div>
+                                        </div>
+                                        <div class="col-auto">
+                                            <i class="fas fa-dumpster-fire fa-2x text-gray-300"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+						
                     </div>
+
                     <!-- Content Row -->
                     <div class="row">
 						<!-- Content Column -->
