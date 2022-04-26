ebe1208613d3647816f71cf422609decc13bd7b1
AdamM-AU <70795583+AdamM-AU@users.noreply.github.com>
2022-04-26 23:58:12 +1000

    Update userMod.php

--- Project Source Code/modules/userMod.php
+++ Project Source Code/modules/userMod.php
@@ -314,6 +314,7 @@ if (isset($_GET["task"])) {
 								<span class="font-weight-bold">Legend:</span><br />
 								<i class="text-warning fa-solid fa-pen"></i> - Edit User<br />
 								<i class="text-danger fa-solid fa-x"></i> - Delete User<br />
+								<i class="text-success fa-solid fa-check"></i> - Undelete User<br />
 								</sub>
 							</div>
                         </div>			
