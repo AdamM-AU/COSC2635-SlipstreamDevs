7ae9ed654f1d7425eb5e8fc5631946c446918eff
idrissbankahulu <idriss@live.com.au>
2022-05-11 14:33:50 +1000

    Update to dashboard screen + assetMod
    - Removed the colour palate thing from the dashboard
    (took a screenshot of the colour code for reference)
    
    - Added asset module file as a placeholder

---
+++ Project Source Code/modules/assetMod.php
@@ -0,0 +1,6 @@
+/* modules/assetMod.php - Asset Management Module
+ * 		Code here takes care of creating, modifying, deleting assets
+ *		Currently a placeholder
+ *
+ * Last Modified: 2022/05/11 By Idris Anwar
+*/
--- Project Source Code/modules/dashboard-home.php
+++ Project Source Code/modules/dashboard-home.php
@@ -81,11 +81,11 @@
                             <!-- Approach -->
                             <div class="card shadow mb-4">
                                 <div class="card-header py-3">
-                                    <h6 class="m-0 font-weight-bold text-primary">Getting Started</h6>
+                                    <h6 class="m-0 font-weight-bold text-primary">Drivers available</h6>
                                 </div>
                                 <div class="card-body">
-                                    <p>Some B.S about getting started with our platform here</p>
-                                    <p class="mb-0">Perhaps even a youtube tutorial???</p>
+                                    <p>Idris attempts to do something idk</p>
+                                    <p class="mb-0">lol</p>
                                 </div>
                             </div>
 						</div>
@@ -107,79 +107,3 @@
 						</div>
 
                     </div>
-                    <!-- Content Row -->
-                    <div class="row">
-					
-                        <!-- Content Column -->
-                        <div class="col-lg-6 mb-4">
-
-                            <!-- Color System -->
-                            <div class="row">
-                                <div class="col-lg-6 mb-4">
-                                    <div class="card bg-primary text-white shadow">
-                                        <div class="card-body">
-                                            Primary
-                                            <div class="text-white-50 small">#4e73df</div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-6 mb-4">
-                                    <div class="card bg-success text-white shadow">
-                                        <div class="card-body">
-                                            Success
-                                            <div class="text-white-50 small">#1cc88a</div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-6 mb-4">
-                                    <div class="card bg-info text-white shadow">
-                                        <div class="card-body">
-                                            Info
-                                            <div class="text-white-50 small">#36b9cc</div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-6 mb-4">
-                                    <div class="card bg-warning text-white shadow">
-                                        <div class="card-body">
-                                            Warning
-                                            <div class="text-white-50 small">#f6c23e</div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-6 mb-4">
-                                    <div class="card bg-danger text-white shadow">
-                                        <div class="card-body">
-                                            Danger
-                                            <div class="text-white-50 small">#e74a3b</div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-6 mb-4">
-                                    <div class="card bg-secondary text-white shadow">
-                                        <div class="card-body">
-                                            Secondary
-                                            <div class="text-white-50 small">#858796</div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-6 mb-4">
-                                    <div class="card bg-light text-black shadow">
-                                        <div class="card-body">
-                                            Light
-                                            <div class="text-black-50 small">#f8f9fc</div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-6 mb-4">
-                                    <div class="card bg-dark text-white shadow">
-                                        <div class="card-body">
-                                            Dark
-                                            <div class="text-white-50 small">#5a5c69</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>						
-					</div>
